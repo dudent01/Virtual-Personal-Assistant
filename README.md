e8c9dab654a4e2c17fe6660cd1b0be62095269ad
Denis Rozhnov <drozhnov4925@bths.edu>
2020-05-21 14:50:02 -0400

    Most functionality added

--- README.md
+++
@@ -1,3 +0,0 @@
-# Virtual-Personal-Assistant
-
-This program is intended to serve as a virtual assistant for a Windows 10 computer.
--- Towards Data Science/Towards Data Science.py
+++ Towards Data Science/Towards Data Science.py
@@ -14,6 +14,7 @@ import urllib
 import json
 from bs4 import BeautifulSoup as soup
 #from urllib2 import urlopen
+from urllib.request import urlopen
 import wikipedia
 import random
 from time import strftime
@@ -69,6 +70,92 @@ def assistant(command):
             sofiaResponse('The website you have requested has been opened for you Sir.')
         else:
             pass
+
+    elif 'current weather' in command:
+        reg_ex = re.search('current weather in (.*)', command)
+        if reg_ex:
+            try:
+                city = reg_ex.group(1)
+                owm = OWM(API_key='ab0d5e80e8dafb2cb81fa9e82431c1fa')
+                obs = owm.weather_at_place(city)
+                w = obs.get_weather()
+                k = w.get_status()
+                x = w.get_temperature(unit='celsius')
+                sofiaResponse(
+                    'Current weather in %s is %s. The maximum temperature is %0.2f and the minimum temperature is %0.2f degree celcius' % (
+                    city, k, x['temp_max'], x['temp_min']))
+            except:
+                sofiaResponse("You must say current weather in city")
+
+    elif 'time' in command:
+        import datetime
+        now = datetime.datetime.now()
+        sofiaResponse('Current time is %d hours %d minutes' % (now.hour, now.minute))
+
+    elif 'hello' in command:
+        day_time = int(strftime('%H'))
+        if day_time < 12:
+            sofiaResponse('Hello Sir. Good morning')
+        elif 12 <= day_time < 18:
+            sofiaResponse('Hello Sir. Good afternoon')
+        else:
+            sofiaResponse('Hello Sir. Good evening')
+
+    elif 'news for today' in command or 'news' in command:
+        try:
+            news_url = "https://news.google.com/news/rss"
+            Client = urlopen(news_url)
+            xml_page = Client.read()
+            Client.close()
+            soup_page = soup(xml_page, "xml")
+            news_list = soup_page.findAll("item")
+            for news in news_list[:15]:
+                sofiaResponse(str(news.title.text.encode('utf-8')))
+        except Exception as e:
+            print(e)
+
+    elif 'tell me about' in command:
+        reg_ex = re.search('tell me about (.*)', command)
+        try:
+            if reg_ex:
+                topic = reg_ex.group(1)
+                ny = wikipedia.page(topic)
+                sofiaResponse(str(ny.content[:1000].encode('utf-8')))
+        except Exception as e:
+            sofiaResponse(e)
+
+    '''elif 'play me a song' in command:
+        path = 'C:/Users/Denis/Personal Projects/Virtual Personal Assistant'
+        folder = path
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+        sofiaResponse('What song shall I play Sir?')
+        mysong = myCommand()
+        if mysong:
+            flag = 0
+            url = "https://www.youtube.com/results?search_query=" + mysong.replace(' ', '+')
+            response = urllib2.urlopen(url)
+            html = response.read()
+            soup1 = soup(html, "lxml")
+            url_list = []
+            for vid in soup1.findAll(attrs={'class': 'yt-uix-tile-link'}):
+                if ('https://www.youtube.com' + vid['href']).startswith("https://www.youtube.com/watch?v="):
+                    flag = 1
+                    final_url = 'https://www.youtube.com' + vid['href']
+                    url_list.append(final_url)
+                    url = url_list[0]
+            ydl_opts = {}
+            os.chdir(path)
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            vlc.play(path)
+            if flag == 0:
+                sofiaResponse('I have not found anything in Youtube ')'''
 
 while True:
     assistant(myCommand())
